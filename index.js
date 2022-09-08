c4e2f3b0290af54616d81bf13e1a9b402f388e30
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-09-08 09:50:30 +0000

    Fixed javascript linter errors in the index.js file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const toggleBtn = document.querySelector('#toggleBtn');
 const ulElem = document.querySelector('#menuList');
 const projectContainer = document.querySelector('#project-list');
 const modalContainer = document.querySelector('#detail');
-const modalMainContainer = document.querySelector('#detail1')
+const modalMainContainer = document.querySelector('#detail1');
 const lists = document.querySelectorAll('#menuList li a');
 const closeBtn = document.querySelector('#btn_close');
 const blurGround = document.querySelector('.blurBg');
@@ -53,7 +53,7 @@ const projectArray = [
     technologies: ['css', 'html', 'javascript', 'Ruby'],
     linkDemo: '',
     sourceCode: 'https://github.com/til2to/porfolio_projects/tree/main',
-  }
+  },
 ];
 
 projectArray.forEach((project, index) => {
@@ -88,20 +88,20 @@ projectArray.forEach((project, index) => {
 
       <button class="button" id=${index}>See Projects</button>
     </section>
-  </section>`
+  </section>`;
 
   projectContainer.innerHTML += projectItem;
 });
 
-const projectBtns = document.querySelectorAll('.button')
+const projectBtns = document.querySelectorAll('.button');
 
 const modalBtn = document.querySelector('#modal-close');
 
 modalBtn.addEventListener('click', () => {
   modalMainContainer.style.display = 'none';
-  blurGround.style.filter = 'blur(0)'
+  blurGround.style.filter = 'blur(0)';
   blurGround.style.opacity = '1';
-})
+});
 
 const modalContent = (project) => {
   // blurGround.style.filter = 'blur(2px)';
@@ -134,12 +134,12 @@ const modalContent = (project) => {
 `;
   modalContainer.innerHTML = modal;
   modalMainContainer.style.display = 'block';
-}
+};
 
-projectBtns.forEach((projectBtn, index) => {
-  projectBtn.addEventListener('click', (e)=>{
-    const getBtnId = e.target.getAttribute('id')
-    const projectObj = projectArray[getBtnId]
+projectBtns.forEach((projectBtn) => {
+  projectBtn.addEventListener('click', (e) => {
+    const getBtnId = e.target.getAttribute('id');
+    const projectObj = projectArray[getBtnId];
     modalContent(projectObj);
-  })
-})
+  });
+});
